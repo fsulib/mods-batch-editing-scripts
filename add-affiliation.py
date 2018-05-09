a6ec8dfaeb8105234b7dc1def8b05107603c714a
Bryan J. Brown <bjbrown@fsu.edu>
2018-05-09 17:28:12 -0400

    add-affiliation.py doesn't pretty print, but so what

--- add-affiliation.py
+++ add-affiliation.py
@@ -20,11 +20,23 @@ for file in files:
     try:
       mods = ET.parse(targetdir + "/" + file)
       namespaces = {'mods': 'http://www.loc.gov/mods/v3'}
+      ET.register_namespace('', "http://www.loc.gov/mods/v3")
+      ET.register_namespace('mods', "http://www.loc.gov/mods/v3")
+      ET.register_namespace('etd', "http://www.ndltd.org/standards/metadata/etdms/1.0/")
+      ET.register_namespace('flvc', "info:flvc/manifest/v1")
       root = mods.getroot()
+
+      # Find name element of the author
       for name in root.findall('mods:name', namespaces):
-        print(name)
+        for role in name.findall('mods:role', namespaces):
+          for roleTerm in role.findall('mods:roleTerm', namespaces):
+            if roleTerm.attrib['type'] == 'text' and roleTerm.text == 'author':
+              affiliation_node = ET.Element("{http://www.loc.gov/mods/v3}affiliation")
+              affiliation_node.text = affiliation
+              name.append(affiliation_node)
 
-
+      mods.write(processeddir + "/" + file)
+    
     except:
       print(file + " is not wellformed. Please fix.")
       sys.exit()
